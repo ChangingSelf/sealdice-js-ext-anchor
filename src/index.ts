a9e84b6c1a7673e91f447942bfcc0524c0250a9a
ChangingSelf <ChangingSelf@163.com>
2023-02-19 08:36:22 +0800

    v1.1.0添加多个选项

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ const HELP = `群内安价收集(ak是アンカー罗马字缩写)
 .ak help //查看帮助
 .ak # 标题 //新建一轮分歧并设标题
 .ak + 选项 //需要添加的选项的内容
+.ak + 选项1|选项2|选项3 //多个选项用|分隔
 .ak - 序号 //需要移除的选项的序号
 .ak ? //列出目前所有选项
 .ak = //随机抽取1个选项并继续
@@ -30,7 +31,7 @@ function akAdd(ctx: seal.MsgContext, msg: seal.Message, ext: seal.ExtInfo, optio
     options: string[];
   } = JSON.parse(ext.storageGet(STORAGE_KEY) || '{"title":"","options":[]}');
 
-  data.options.push(option);
+  data.options = data.options.concat(option.split("|"));
   seal.replyToSender(ctx, msg, `当前分歧:${data.title}\n已添加第${data.options.length}个选项:${option}`);
 
   ext.storageSet(STORAGE_KEY, JSON.stringify(data));
@@ -81,9 +82,9 @@ function akList(ctx: seal.MsgContext, msg: seal.Message, ext: seal.ExtInfo) {
 
 /**
  * 生成随机整数
- * @param min 
- * @param max 
- * @returns [min,max]
+ * @param min 最小值
+ * @param max 最大值
+ * @returns 位于[min,max]区间的随机整数
  */
 function randomInt(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -117,7 +118,7 @@ function main() {
   // 注册扩展
   let ext = seal.ext.find('anchor');
   if (!ext) {
-    ext = seal.ext.new('anchor', '憧憬少', '1.0.0');
+    ext = seal.ext.new('anchor', '憧憬少', '1.1.0');
     seal.ext.register(ext);
   }
 
